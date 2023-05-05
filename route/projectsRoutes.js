6d20880af08ff27bef18b31db56b606cc72c4afa
kirubel <kirato747@gmail.com>
2023-05-05 19:37:39 +0300

    backend memory optimization v1

--- route/projectsRoutes.js
+++ route/projectsRoutes.js
@@ -2,23 +2,24 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controller/projectsController');
 const multer = require('multer');
-const {storage} = require("../cloudinary");
+const { storage } = require("../cloudinary");
+
 // Multer middleware configuration
 const multerFilter = (req, file, cb) => {
-
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
-  }  else {
-    cb(new AppErorr("Please Enter Valid file type", 400));
+  } else {
+    cb(new Error("Please Enter Valid file type"));
   }
 };
+
 const upload = multer({
   storage: storage,
   fileFilter: multerFilter,
   limits: {
     fileSize: 1024 * 1024 * 5 // 5 MB file size limit
   },
-}); 
+});
 
 // Create new project
 router.post('/', upload.single('coverImage'), projectController.createProject);
@@ -34,5 +35,11 @@ router.put('/:id', upload.single('coverImage'), projectController.updateProjectB
 
 // Delete project by ID
 router.delete('/:id', projectController.deleteProjectById);
+
+// Log unhandled errors
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Server error' });
+});
 
 module.exports = router;

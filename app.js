958df3db2a84d450a37b17fd6a464a1cbe4f2c0a
kirubel <kirato747@gmail.com>
2023-05-05 18:36:44 +0300

    backend memory optimization

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const blogRoutes = require('./route/blogRoutes');
 const categoryRoutes = require('./route/categoryRoutes');
 const projectsRoutes = require('./route/projectsRoutes');
 const emailRoutes = require('./route/emailRoutes');
-const AppError=require('./utils/AppError')
+// const AppError=require('./utils/AppError')
 
 
 
@@ -27,7 +27,7 @@ app.all("*", (req, res, next) => {
   // const err = new Error("ther os a errrasd")
   // err.statusCode=404
   // err.status="fial"
-  return next(new AppError("Page is not found", 404));
+  // return next(new AppError("Page is not found", 404));
 });
 
 module.exports = app;

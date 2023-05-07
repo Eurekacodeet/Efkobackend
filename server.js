01283d69490f9fe2fba179ad40f84dc2d2d4442a
kirubel <kirato747@gmail.com>
2023-05-07 09:12:25 +0300

    db store fix port change again

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ mongoose
     console.log("Db connection successful! ..........");
   });
 
-app.listen(5000, () => {
+app.listen(6000, () => {
   console.log("the server is running........");
 });
